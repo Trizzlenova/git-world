b579e07dc32906f4ef9239bf5c7b6bc4b63eb337
Troy Swayzee <tswayzee@Troys-MacBook-Pro.local>
2021-11-23 11:44:25 -0800

    started game layout

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+$('#container').hover(
+    function() {
+        $(this).css({color: 'white'}); //mouseover
+    },
+    function() {
+        $(this).css({color: 'black'}); //mouseout
+    }
+);
+
+$('.nes-smartphone').css({'': '1000px'})
